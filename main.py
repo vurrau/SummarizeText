f658c9537422dc68663f196884d52d1688101ba7
vurrau <podvishevskiy@gmail.com>
2024-06-19 11:17:37 +0300

    Summarize v2

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from fastapi import FastAPI
 import uvicorn
-from transformers import pipeline
+from fastapi import FastAPI
+from langchain_huggingface.llms import HuggingFacePipeline
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
 
 app = FastAPI()
+
+model_id = "facebook/bart-large-cnn"  # Specify the model and the task
+task = "summarization"
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)  # Load the tokenizer and model
+model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
+
+pipe = pipeline(task, model=model, tokenizer=tokenizer)  # Create pipeline
+hf_pipeline = HuggingFacePipeline(pipeline=pipe)  # Create an instance of HuggingFacePipeline specifying a pipeline
 
 
 @app.post("/summarize")
 async def summarize(request: str):
-    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")  # AI by Hugging Face
-    summary = summarizer(request, max_length=130, min_length=30, do_sample=False)  # cfg AI
+    summary = hf_pipeline(request)
     return {"summary": summary}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
