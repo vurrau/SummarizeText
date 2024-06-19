07c7d276e16e5b6d60a7f548302ac204886cbcff
vurrau <podvishevskiy@gmail.com>
2024-06-19 12:06:43 +0300

    Summarize v3

--- README.md
+++ README.md
@@ -14,11 +14,11 @@ source env/bin/activate  # On Windows use `env\Scripts\activate`
 ```
 ### 2. Install dependencies:
 ```
-pip install fastapi uvicorn langchain langchain-huggingface transformers huggingface_hub
+pip install fastapi uvicorn langchain-huggingface transformers
 ```
 ### 3. Run the application:
 ```
-uvicorn main:app --reload
+uvicorn main:app --reloa
 ```
 ### 4. Test the endpoint:
 - Send a POST request to ```http://127.0.0.1:8000/summarize``` with a JSON body containing the text to be summarized.
--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
 from langchain_huggingface.llms import HuggingFacePipeline
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
 
 app = FastAPI()
 
-model_id = "facebook/bart-large-cnn"  # Specify the model and the task
-task = "summarization"
-
-tokenizer = AutoTokenizer.from_pretrained(model_id)  # Load the tokenizer and model
-model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
-
-pipe = pipeline(task, model=model, tokenizer=tokenizer)  # Create pipeline
-hf_pipeline = HuggingFacePipeline(pipeline=pipe)  # Create an instance of HuggingFacePipeline specifying a pipeline
+# Create a HuggingFacePipeline for the summarization model
+hf_pipeline = HuggingFacePipeline.from_model_id(
+    model_id="facebook/bart-large-cnn",
+    task="summarization",
+    pipeline_kwargs={"max_new_tokens": 150},
+)
 
 
 @app.post("/summarize")
@@ -22,4 +19,3 @@ async def summarize(request: str):
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
-
